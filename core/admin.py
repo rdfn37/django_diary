34e98a600f4e60933334b3f357593991986663d0
rdfn37 <88458654+rdfn37@users.noreply.github.com>
2022-04-12 18:05:10 -0300

    Tabela de Eventos

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from core.models import Event
 # Register your models here.
+
+class AdminEvent(admin.ModelAdmin):
+    list_display = ('title', 'event_date', 'creation_date')
+    list_filter = ('user',)
+
+admin.site.register(Event, AdminEvent)
